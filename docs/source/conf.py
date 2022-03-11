a64d32595c0b5487727c90a2f66dd69e5b417893
bendichter <ben.dichter@gmail.com>
2022-03-11 15:47:12 -0600

    add favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,10 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_favicon = 'img/favicon_96.png'
+
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+

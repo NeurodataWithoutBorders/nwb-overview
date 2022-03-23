f1ee3cdcd506597135c17bd6e52a872455000eb3
Ryan Ly <rly@lbl.gov>
2022-03-22 21:05:38 -0700

    Use sphinx rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.dirname(__file__))
 # Import the definition of external links
 from conf_extlinks import extlinks
 from conf_extlinks import intersphinx_mapping
+import sphinx_rtd_theme
 
 
 project = 'NWB Overview'
@@ -20,14 +21,15 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    #'sphinx.ext.duration',
+    # 'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'myst_parser',
     'sphinx.ext.extlinks',
-    'sphinx_design'
+    'sphinx_design',
+    'sphinx_rtd_theme',
 ]
 
 intersphinx_disabled_domains = ['std']
@@ -35,8 +37,8 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'

acd43d2ab3092890203d1ce756a36ed4c321e74f
Ryan Ly <rly@lbl.gov>
2025-01-14 14:04:06 -0800

    Set theme back to sphinx_rtd_theme (#184)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,6 +51,8 @@ html_favicon = 'img/favicon_96.png'
 # html_logo = None
 html_logo = 'img/logo_brain_transp.png'
 
+html_theme = "sphinx_rtd_theme"
+
 html_theme_options = {
     # 'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
     # 'analytics_anonymize_ip': False,
